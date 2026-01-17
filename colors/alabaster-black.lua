effeeaf0d5d6337dcfd78686311b79172ac7402f
Jynn Nelson <jynn.nelson@ferrous-systems.com>
2026-01-17 15:54:30 -0500

    Conceal works properly even when dim_comments is disabled

--- colors/alabaster-black.lua
+++ colors/alabaster-black.lua
@@ -71,7 +71,7 @@ if vim.o.background == "dark" then
     theme = {
         Comment = { fg = comment_fg },
         ColorColumn = { bg = "#182325" },
-        Conceal = { fg = comment_fg },
+        Conceal = { fg = dim_comment },
         Cursor = { bg = active, fg = "#000000" },
         -- lCursor      { }, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
         -- CursorIM     { }, -- like Cursor, but used when in IME mode |CursorIM|
